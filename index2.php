57c8ac25834106a1f26b3d7e6baf1c09e0e0a5d4
Faty <fadiakhate82@gmail.com>
2025-02-25 16:28:12 +0000

    Ajout d'un paragraphe

---
+++ index2.php
@@ -0,0 +1,15 @@
+<h1>Gestion de la bibliothèque</h1>
+<nav>
+    <ul>
+        <li><a href="articles.php">Gestion des articles</a></li>
+        <li><a href="membres.php">Gestion des membres</a></li>
+        <li><a href="emprunts.php">Gestion des emprunts</a></li>
+        <li><a href="comptes.php">Gestion des comptes</a></li>
+        <li><a href="livres.php">Gestion des livres</a></li>
+        <li><a href="cds.php">Gestion des CDs</a></li>
+        <li><a href="dvds.php">Gestion des DVDs</a></li>
+    </ul>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga corrupti debitis,
+         veritatis eligendi earum molestias, eaque voluptatem sunt maxime labore, 
+        perferendis maiores ea iure odit ipsam dolor aperiam culpa! Ratione?</p>
+</nav>
